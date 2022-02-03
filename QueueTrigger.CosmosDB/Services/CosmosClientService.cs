06a4978a647a55abfe546f0163a04fe9453b480e
Mykola Khandus <mkhandus@deloitte.com>
2022-02-03 12:08:47 +0200

    CosmosClientService

---
+++ QueueTrigger.CosmosDB/Services/CosmosClientService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Configuration;
+
+namespace QueueTrigger.CosmosDB.Services
+{
+    public class CosmosClientService
+    {
+        private readonly IConfiguration configuration;
+
+        public CosmosClientService(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public CosmosClient GetCosmosClient()
+        {
+            return new CosmosClient("");
+        }
+    }
+}
